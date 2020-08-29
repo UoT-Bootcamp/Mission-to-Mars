1ea32aac22766ef0f0731d77aa04e760a86c2f3d
preeti0103 <64617207+preeti0103@users.noreply.github.com>
2020-08-28 21:38:47 -0400

    Code cleanup in app.py file

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -1,3 +1,4 @@
+# Import dependencies
 from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
 import scrape_mars
@@ -6,22 +7,28 @@ import pymongo
 # Create an instance of Flask
 app = Flask(__name__)
 
-# Use PyMongo to establish Mongo connection
+# Use flask_pymongo to establish Mongo connection
 app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_facts_app"
 mongo = PyMongo(app)
+
 
 client = pymongo.MongoClient()
 db = client.mars_db
 collection = db.mars_facts
 
 
-
 @app.route("/scrape")
 def scrape():
+
     mars_data = mongo.db.mars_data
+
+    # Run the scrape function
     mars_database = scrape_mars.scrape()
+
     # Update the Mongo database using update and upsert=True
     mars_data.update({}, mars_database, upsert=True)
+
+    # Redirect back to home page
     return redirect("/", code = 302)
 
 # Route to render index.html template using data from Mongo
@@ -36,4 +43,3 @@ def home():
 
 if __name__ == "__main__":
     app.run(debug = True)
-
